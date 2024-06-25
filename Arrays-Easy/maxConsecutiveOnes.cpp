0c48d34412b61f2c93787c266f20c7228660db1e
Megha <meghabansal1230@gmail.com>
2024-06-25 19:15:06 +0530

    Add files via upload

---
+++ Arrays-Easy/maxConsecutiveOnes.cpp
@@ -0,0 +1,12 @@
+//    int maxCon = INT_MIN;
+//         int count = 0;
+        
+//         for(int i = 0; i < nums.size(); i++) {
+//             if(nums[i] == 1) {
+//                 count++;
+//             } else {
+//                 count = 0;
+//             }
+//             maxCon = max(maxCon, count);
+//         }
+//         return maxCon;
---
+++ Arrays-Easy/single_number.cpp
@@ -0,0 +1,4 @@
+// int ans=0;
+// 	   for(auto x:nums)
+// 	   ans^=x;
+// 	   return ans;
