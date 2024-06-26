d752ed3e0596b85c9d360dd6d36d8ef21d8d83b9
Megha <meghabansal1230@gmail.com>
2024-06-26 19:11:55 +0530

    Add files via upload

---
+++ Arrays-Easy/rotateArrayToRightByK.cpp
@@ -0,0 +1,12 @@
+//  void rotate(vector<int>& nums, int k) {
+//         int n =nums.size();
+//         int arr[n];
+//          k=k%n;
+//         vector<int> b;
+//         for(int i=n-k;i<n;i++)
+//             b.push_back(nums[i]);
+//         for(int i=0;i<n-k;i++)
+//             b.push_back(nums[i]);
+//         for(int i=0;i<n;i++)
+//             nums[i]=b[i];
+//     }
