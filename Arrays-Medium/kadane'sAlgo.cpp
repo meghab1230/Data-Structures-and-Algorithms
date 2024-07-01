488dd43f00664c825b880e4a4dc43f6c25132c00
Megha <meghabansal1230@gmail.com>
2024-07-01 18:39:45 +0530

    Add files via upload

---
+++ Arrays-Medium/kadane'sAlgo.cpp
@@ -0,0 +1,15 @@
+//  int maxSubArray(vector<int>& nums) {
+//         int sum=0;
+//         int maxi=nums[0];
+//         int n=nums.size();
+
+//         for(int i =0;i<n;i++){
+//             sum=sum+nums[i];
+//             maxi=max(maxi,sum);
+
+//             if(sum<0){
+//                 sum=0;
+//             }
+//         }
+//         return maxi;
+//     }s
